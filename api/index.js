ca50a6b16813039469e1392b9c8ccf4ee391ca2c
Janara Tolonbaeva <janaratolonbaeva@gmail.com>
2021-05-04 17:46:37 +0600

    add HW90

---
+++ api/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const {nanoid} = require('nanoid');
+const app = express();
+require ('express-ws')(app);
+
+app.use(cors());
+
+const port = 8000;
+
+const activeConnections = {};
+
+app.ws('/canvas', function (ws, req) {
+  const id = nanoid();
+  console.log('client connected! id=', id);
+  activeConnections[id] = ws;
+
+  ws.on('message', msg => {
+    console.log('Message received!', msg);
+    const decoded = JSON.parse(msg)
+
+    if (decoded.type === 'CREATE_CANVAS') {
+      Object.keys(activeConnections).forEach(key => {
+        const connection = activeConnections[key];
+
+        connection.send(JSON.stringify({
+          type: 'NEW_CANVAS',
+          canvas: decoded.canvas
+        }))
+      })
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('client disconnected! id=', id);
+    delete activeConnections[id];
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port!`);
+});
+
+
+
---
+++ frontend/src/App.js
@@ -0,0 +1,80 @@
+import React, {useState, useRef, useEffect} from 'react';
+
+const App = () => {
+  const ws = useRef(null);
+  const [state, setState] = useState({
+    mouseDown: false,
+    pixelsArray: []
+  });
+
+  useEffect(() => {
+    ws.current = new WebSocket('ws://localhost:8000/canvas');
+
+    ws.current.onmessage = e => {
+      const decoded = JSON.parse(e.data);
+
+      if (decoded.type === 'NEW_MESSAGE') {
+        setState(prev => [
+          ...prev,
+          {canvas: decoded.canvas}
+        ])
+      }
+    };
+
+  }, []);
+
+  const canvas = useRef(null);
+
+  const canvasMouseMoveHandler = event => {
+    if (state.mouseDown) {
+      const clientX = event.clientX;
+      const clientY = event.clientY;
+      setState(prevState => {
+        return {
+          ...prevState,
+          pixelsArray: [...prevState.pixelsArray, {
+            x: clientX,
+            y: clientY
+          }]
+        };
+      });
+
+      const context = canvas.current.getContext('2d');
+      const imageData = context.createImageData(1, 1);
+      const d = imageData.data;
+
+      d[0] = 0;
+      d[1] = 0;
+      d[2] = 0;
+      d[3] = 255;
+
+      context.putImageData(imageData, event.clientX, event.clientY);
+    }
+  };
+
+  const mouseDownHandler = event => {
+    setState({...state, mouseDown: true});
+  };
+
+  const mouseUpHandler = event => {
+    ws.current.send(JSON.stringify({type: 'CREATE_CANVAS', canvas: state.pixelsArray}));
+    
+    setState({...state, mouseDown: false, pixelsArray: []});
+  };
+
+  return (
+    <div style={{marginBottom: '20px'}}>
+      <canvas
+        ref={canvas}
+        style={{border: '1px solid black'}}
+        width={800}
+        height={600}
+        onMouseDown={mouseDownHandler}
+        onMouseUp={mouseUpHandler}
+        onMouseMove={canvasMouseMoveHandler}
+      />
+    </div>
+  );
+};
+
+export default App;
